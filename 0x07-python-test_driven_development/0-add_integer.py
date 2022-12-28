a51542eca64fc600b1f38187374f00bf8cc2ad88
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-12-28 11:30:34 +0200

    task for 0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Add integer module"""
+
+
+def add_integer(a, b=98):
+    """Adds integers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
