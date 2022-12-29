e67d9b8c057f20baefaa93b1182b0b2c9da58223
Eng Nyemba Martin <martinnyemba@gmail.com>
2022-12-29 19:40:51 +0200

    module a function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""function or module that prints a square with the character #"""
+
+
+def print_square(size):
+    """Print square function"""
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
