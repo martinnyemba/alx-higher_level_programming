9f3eb0212c1d498b6ac5de291d7b5caf3e80b196
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-12 18:22:14 +0000

    Fix task 2 and 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-#  program that prints the ASCII alphabet, in lowercase
-#  not followed by a new line
-
-# use a for loop to iterate from 97 to 122
 for i in range(97, 123):
-    if i != 101 and i != 113:
-
-        #  print character using char strinf format
-        print("{:c}" .format(i), end="")
+    if chr(i) != 'q' and chr(i) != 'e':
+        print('{}'.format(chr(i)), end='')
