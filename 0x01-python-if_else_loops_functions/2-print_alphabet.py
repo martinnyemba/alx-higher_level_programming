e02f27cf38e3503961b8c9e581d455162f192a2a
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-12 17:41:10 +0000

    Add  : 2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#  program that prints the ASCII alphabet, in lowercase
+#  not followed by a new line
+
+# use a for loop to iterate from 97 to 122
+for i in range(97, 123):
+
+    #  print character using chr
+    print(chr(i), end="")
