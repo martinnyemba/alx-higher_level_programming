57b8158236a6c78cbb18b1efd7f11adc50808fa4
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-13 10:13:27 +0000

    Add: File: 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def remove_char_at(str, n):
     """function that creates a copy of the string
-    removing the character at the position.
+    And removing the character at the position.
     """
     if n < 0 or n >= len(str):
         return (str)
