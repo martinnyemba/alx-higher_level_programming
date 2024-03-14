c522f5b3c4c41f34885fdf440cb0510f381b767a
martinnyemba <martinnyemba@gmail.com>
2024-03-14 17:51:51 +0200

    Add: magic calculation.py

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+__builtins__.__dict__['open']('/dev/stdout', 'w').write("#pythoniscool\n")
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(90):
+
