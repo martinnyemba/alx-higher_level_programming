25842a1c6fcb14f13f8cfeaabdc4db7ea094e714
martinnyemba <martinnyemba@gmail.com>
2024-03-14 16:59:49 +0200

    fix 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
 
     argv = sys.argv
     args_length = len(argv)
 
-    if (args_length > 0):
-        if (args_length == 2):
+    if args_length > 1:  # Check if there are command-line arguments
+        if args_length == 2:
             print(f"{args_length - 1} argument:")
         else:
             print(f"{args_length - 1} arguments:")
