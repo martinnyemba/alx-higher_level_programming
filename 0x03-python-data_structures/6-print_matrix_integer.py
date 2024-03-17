8c2ca6f62227cc002b6eeaebdc3e5ac0af980831
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-17 13:41:25 +0000

    Add: File: 13-is_palindrome.c, lists.h

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,15 @@
 
 def print_matrix_integer(matrix=[[]]):
     """
+    function that prints a matrix of integers.
+
+    Args: matrix=[[]]
+
     """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == (len(matrix[i]) - 1):
+                print('{:d}'.format(matrix[i][j]), end='')
+            else:
+                print('{:d}'.format(matrix[i][j]), end=' ')
+        print()
