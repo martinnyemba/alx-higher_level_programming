93e61116d71634c6dd0e957383ef0d2cb95b9aa9
martinnyemba <martinnyemba@gmail.com>
2024-03-17 07:17:23 +0200

    Add - File: 4-new_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,10 +5,10 @@ def replace_in_list(my_list, idx, element):
     function that replaces an element of a list at a specific position
     Prototype: def replace_in_list(my_list, idx, element):
 
-    If idx is negative, and returns the original list
-    If idx is out of range  returns the original list
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
         return my_list
     else:
         my_list[idx] = element
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,4 +6,6 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list):
         return my_list
     else:
-
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
