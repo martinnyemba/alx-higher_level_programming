59214f11ac52635566f775251eab7a8931ea8482
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-20 20:12:26 +0000

    fix: 13. Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if(my_list):
-        return(sum(x*y for x, y in my_list) / sum(y for x, y in my_list))
+    """
+    function that returns the weighted average of all
+    integers tuple (<score>, <weight>)
+    """
+    if (my_list):
+        return (sum(x*y for x, y in my_list) / sum(y for x, y in my_list))
     else:
-        return(0)
+        return (0)
