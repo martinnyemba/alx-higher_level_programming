352de929c152a3f12db1c206a4d9feb8a55508e1
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-03-20 20:04:09 +0000

    Add : File 11 to File: 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+
+    Args:
+        roman_string (str): The Roman numeral string to be converted.
+
+    Returns:
+        int: The integer representation of the Roman numeral.
+    """
+    if roman_string is None:
+        return 0
+    if type(roman_string) is not str:
+        return 0
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number-1, 0, -1):
+        current = romans[roman_string[i]]
+        prev = romans[roman_string[i-1]]
+        if prev >= current:
+            value_int += prev
+        else:
+            value_int -= prev
+    return value_int
