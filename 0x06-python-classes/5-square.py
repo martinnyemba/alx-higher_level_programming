65fe96fe29645d416c081cef94e8f5f1a2e81eb4
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-04-03 20:20:54 +0000

    file add 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""Ddefine Sqaure class"""
+
+
+class Square:
+    """  a class Square that represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """ Define a getter method"""
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """define a setter method"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ Public instance method that prints in stdout
+        the square with the character #: """
+
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
