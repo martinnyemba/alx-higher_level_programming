7a663ac362120779bbf7849d32aff365f119db00
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-04-03 18:35:57 +0000

    Fix : Documentation- File: 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,11 @@
 
 class Square:
     """a class Square that defines a square"""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
