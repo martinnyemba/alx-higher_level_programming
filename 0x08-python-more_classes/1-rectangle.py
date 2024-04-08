c70585b8e48373ddb617fbc42fba87a23c4e37cc
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-04-08 20:24:34 +0000

    1-main.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,27 +3,36 @@
 """Module - that defines a rectangle"""
 
 
-class Rectangle:
-    """class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
-    """
-    # Getter method for width property
+class Rectangle():
+    """class Rectangle that defines a rectangle"""
+    #  Getter method for width property
     @property
     def width(self):
         return self.__width
-    # Setter method for width property
+
+    #  Setter method for width property
     @width.setter
     def width(self, value):
-        return self.__width = value
-    # Instantiation of main method with optional: width and height
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
 
+    #  property getter for height
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    #  Instantiation of main method with optional: width and height
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    def area(self):
-        return (self.width * self.height)
-    def perimeter(self):
-        
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod('1-rectangle_test.txt')
