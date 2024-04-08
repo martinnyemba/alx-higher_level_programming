71a31c871db3f3165fdc2477db3823809f2ac836
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-04-08 14:29:25 +0000

    Add(File: 0-rectangle.py) and Test cases

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Module - that defines a rectangle"""
+
+
+class Rectangle:
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod('tests/0-rectangle_test.txt')
