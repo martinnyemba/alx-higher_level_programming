d62959db2409065d5d407f9e6606b79154956193
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-04-10 08:39:44 +0000

    Add: File: 5-text_indentation.py, tests/5-text_indentation.txt

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Text indentation module that prints a text with 2 new lines after each of ., ? and : """
+
+
+def text_indentation(text):
+    """Text indentation function"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    special_chars = ['.', '?', ':']
+    length = len(text)
+    skip = None
+    for i, char in enumerate(text, 1):
+        if skip:
+            print("\n")
+            skip = None
+        else:
+            print(char, end="")
+        if char in special_chars:
+            skip = 1
+        if i == length:
+            last_char = char
+
+    if last_char in special_chars:
+        print("\n")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
