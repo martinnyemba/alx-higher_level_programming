78aa81f4dc7e6ccc69ea0a808cd801fe20581b6b
martinnyemba <martinnyemba@gmail.com>
2024-04-22 16:45:10 +0200

    Update: the class Square by adding the public method def update(self, *args, **kwargs) that assigns attributes; File: models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the Square (a special Rectangle)"""
-from models.rectangle import Rectangle 
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
@@ -28,7 +28,56 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        Update the values of the Square attributes.
+        This method allows updating multiple attributes of the Square
+        object at once.
+        It accepts positional arguments (args) or keyword arguments (kwargs).
+
+        Args:
+            *args: Positional arguments representing the attributes in the
+            order id, size, x, y.
+            **kwargs: Keyword arguments representing the attributes with
+            their respective names.
+        """
+        length = len(args)
+        if length:  # checks if there are any positional arguments
+            # iterates over the positional arguments using enumerate
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+
+        #  If there are no positional arguments (length is 0)
+        #  checks the keyword arguments (kwargs) using if "key" in kwargs
+        else:
+            if "id" in kwargs:
+                #  retrieve value associated with specified key from kwargs
+                self.id = kwargs.get("id")
+            if "size" in kwargs:
+                self.size = kwargs.get("size")
+            if "x" in kwargs:
+                self.x = kwargs.get("x")
+            if "y" in kwargs:
+                self.y = kwargs.get("y")
+
     def __str__(self):
-        """ magic method __str__ """
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                self.y, self.size))
+        """
+        Return a string representation of the Square object.
+        This magic method __str__ is automatically called when using
+        the str() function or when an object is converted to a string
+        (e.g., through print).
+
+        Returns:
+            str: A string representing the Square object in the
+            format "[Square] (id) x/y - size".
+            Here, 'id' is the unique identifier, 'x' and 'y' are the
+            coordinates, and 'size' is the size of the square.
+        """
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
