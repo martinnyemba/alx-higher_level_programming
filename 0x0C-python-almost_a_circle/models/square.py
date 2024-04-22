7d6ed17e2ef36fec1c6e616de0402d5504164561
martinnyemba <martinnyemba@gmail.com>
2024-04-22 10:14:41 +0200

    Update: __str__ method overiding

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """Defines the Square (a special Rectangle)"""
+from model.rectangle import Rectangle 
 
 
-class Square(Rectangke):
+class Square(Rectangle):
     """Class Square inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class contructor method
 
         Args:
-            size (int):
-            x (int):
-            y (int)
-            width (int)
-            height (int):
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+
         """
-        super().__init__(id, x, y, width, height)
-        self.size = width, height
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """ magic method __str__ """
