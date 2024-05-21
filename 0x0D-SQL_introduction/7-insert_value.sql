82290e021e96544b57288ab095660a9812f1a9d4
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-05-21 23:26:31 +0200

    File: 8-count_89.sql and File: 7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- inserts a new row in the table named --> (first_table)
+-- The database name passed as an argument of the mysql command
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, "Best School");
