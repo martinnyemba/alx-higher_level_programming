ed9881f0b3b3c369953b205d4e7f116ec46d6404
martinnyemba <martinnyemba@gmail.com>
2024-06-18 21:37:31 +0200

    Added script for the file |6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Define an array of strings
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array using a for...of loop
+for (let line = 0; line < lines.length; line++) {
+  // Print the current line to the console
+  console.log(lines[line]);
+}
