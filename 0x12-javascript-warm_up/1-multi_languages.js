dd22c7c7527c25f6a0a21a5a4088f4a7f9a0736d
martinnyemba <martinnyemba@gmail.com>
2024-06-18 14:35:39 +0200

    Fix semistandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* prints 3 lines */
-console.log("C is fun")
-console.log("Pythonis cool")
-console.log("Javascript is amazing")
+console.log('C is fun');
+console.log('Pythonis cool');
+console.log('Javascript is amazing');
