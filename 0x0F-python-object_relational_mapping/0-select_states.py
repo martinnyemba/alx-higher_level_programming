097a57e58dc0aedd4f4af4e0844404981f53b83e
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-22 16:21:13 +0200

    Correction | 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,16 @@
 lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user="root",
-            passwd="root",
-            db="hbtn_0e_0_usa"
+            user=sys.arg[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
             )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
