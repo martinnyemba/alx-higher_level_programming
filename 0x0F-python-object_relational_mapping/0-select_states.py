7c878e279393bcda01eb5302228d3016d2303705
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-22 13:33:53 +0200

    Update 0-select_states.sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,12 @@ lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 
-conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+conn = MySQLdb.connect(host="loaclhost", user="root", passwd="root", db="hbtn_0e_0_usa")
 cur = conn.cursor()
 
 cur.execute("SELECT * FROM states")
+rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
