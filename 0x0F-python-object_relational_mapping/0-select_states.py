59611b1f4dfaf30933c3ba1fd3a8ed5bc193b21f
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-22 13:45:00 +0200

    Fix errors port number to 3306 - file 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,10 @@ lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="hbtn_0e_0_usa")
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM states")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 rows = cur.fetchall()
 for row in rows:
     print(row)
