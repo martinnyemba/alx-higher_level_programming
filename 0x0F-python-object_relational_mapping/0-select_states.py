7f2b9c59fa9b827cd14320ce37732d9ea46bbdfc
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-22 14:07:41 +0200

    Fix- lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,18 @@ lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            passwd="root",
+            db="hbtn_0e_0_usa"
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
