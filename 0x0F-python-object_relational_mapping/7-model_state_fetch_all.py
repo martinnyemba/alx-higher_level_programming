4e1871b3fe268ee92d387eba93af08709ba0b2ee
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-23 18:44:33 +0200

    Add [file] 7. All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+This script starts a link class to a table in a database
+using SQLAlchemy. It connects to a MySQL database, queries the
+State table and prints each state.
 """
 import sys
 from model_state import Base, State
@@ -8,10 +11,20 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    # create engine that connects to my sql database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Create all tables in the database defined by Base's subclasses
     Base.metadata.create_all(engine)
+
+    # Bind the engine to the session
+    # Create a configured "Session" class and Create a Session
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Query the State table and order by State.id
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
+
+    # Close the Session
+    session.close()
