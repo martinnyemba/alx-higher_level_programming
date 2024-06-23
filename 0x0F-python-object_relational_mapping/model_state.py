e9dbbddef3610fa10dfd231bb77797bd5609c7ea
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-23 17:28:07 +0200

    Add File: model_state.py | Created virtual enviromemnt venv

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,15 +3,18 @@
 Module contains script that contains the class definition of a State
 and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String, Metadata
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base(metadata=Metadata())
+# receive a pre-existing MetaData object for declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
     """
     Class with id and name attributes of each State
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+    id = Column("id", Integer, unique=True, nullable=False, primary_key=True)
+    name = Column("states_name", String(128), nullable=False)
