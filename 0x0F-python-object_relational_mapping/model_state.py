f9896d73cba8508af309e0c15f065f2e305cae4e
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-06-23 17:39:50 +0200

    Fix/Correction of [6. First state model]

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,5 +16,5 @@ class State(Base):
     Class with id and name attributes of each State
     """
     __tablename__ = 'states'
-    id = Column("id", Integer, unique=True, nullable=False, primary_key=True)
-    name = Column("states_name", String(128), nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
